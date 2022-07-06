1a8f5c17e7bdd6ad81aea0f8eb0d100e3c4cbfa0
Trevor Lee <trevorleeoh@gmail.com>
2022-07-06 14:02:30 -0400

    fixed Account model

--- tenmo-client/src/main/java/com/techelevator/tenmo/model/Account.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -11,6 +11,8 @@ public class Account {
 
     private BigDecimal balance;
 
+    public Account() {}
+
     public Account(int accountId, int userId, BigDecimal balance) {
         this.accountId = accountId;
         this.userId = userId;
@@ -29,8 +31,15 @@ public class Account {
         return balance;
     }
 
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
 
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-
-
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
