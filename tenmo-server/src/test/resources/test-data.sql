4fae709ff047bc884c017250b8c405b772dc3ff8
Aubrey Thompson <aubreyannthompson@gmail.com>
2022-07-09 10:18:18 -0400

    fixed account/user id bug

--- tenmo-server/src/test/resources/test-data.sql
+++ tenmo-server/src/test/resources/test-data.sql
@@ -9,17 +9,17 @@ INSERT INTO tenmo_account (user_id, balance) VALUES (1002, 500.00);
 INSERT INTO tenmo_account (user_id, balance) VALUES (1003, 100.00);
 
 INSERT INTO tenmo_transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount)
-VALUES (2, 2, 1001, 1002, 100.00);
+VALUES (2, 2, 2001, 2002, 100.00);
 
 INSERT INTO tenmo_transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount)
-VALUES (1, 1, 1001, 1002, 100.00);
+VALUES (1, 1, 2001, 2002, 100.00);
 INSERT INTO tenmo_transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount)
-VALUES (1, 2, 1001, 1002, 100.00);
+VALUES (1, 2, 2001, 2002, 100.00);
 INSERT INTO tenmo_transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount)
-VALUES (1, 3, 1001, 1002, 100.00);
+VALUES (1, 3, 2001, 2002, 100.00);
 
 INSERT INTO tenmo_transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount)
-VALUES (2, 2, 1001, 1003, 100.00);
+VALUES (2, 2, 2001, 2003, 100.00);
 
 
 
